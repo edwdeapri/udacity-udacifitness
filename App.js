fe9616cc43e89a7b8d1b5b89c9d4c8164cdc94bf
edwdeapri <edward.d.pritchard@gmail.com>
2018-10-31 15:39:22 -0700

    deploy: styling / layout

--- App.js
+++ App.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import { View } from 'react-native';
-import AddEntry from "./components/AddEntry";
+import React from 'react'
+import { View } from 'react-native'
+import AddEntry from './components/AddEntry'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
+import History from './components/History'
 
 export default class App extends React.Component {
-  render() {
-    return (
-        <Provider store={createStore(reducer)}>
-            <View>
-                <AddEntry />
-            </View>
-        </Provider>
-    );
-  }
+    render() {
+        return (
+            <Provider store={createStore(reducer)}>
+                <View style={{flex: 1}}>
+                    <History />
+                </View>
+            </Provider>
+        )
+    }
 }
